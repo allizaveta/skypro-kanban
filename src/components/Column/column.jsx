7847a6c15bd5907c55bcb33f76e8ab69635c66a8
Allizaveta <allizaveta@gmail.com>
2024-05-28 10:21:27 +0300

    structured React application created

---
+++ src/components/Column/column.jsx
@@ -0,0 +1,21 @@
+import Card from "../Card/card";
+
+const Column = ({ title }) => {
+  return (
+    <div className="main__column">
+      <div className="column__title">
+        <p>{title}</p>
+      </div>
+      <div className="cards">
+        <Card category="Research" title="Найти ту кНигу" date="30.12.24" />
+        <Card
+          category="Web Design"
+          title="Заделать карточкуВБ"
+          date="30.12.24"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Column;
